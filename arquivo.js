44de91a52916f256c14c9e23b4f0f336361a0c53
K4dusk <150096775+destruir1573@users.noreply.github.com>
2024-11-02 12:32:04 -0300

    subi commit

---
+++ arquivo.js
@@ -0,0 +1,10 @@
+window.sr = ScrollReveal({reset: true});
+
+sr.reveal('.oi',{
+    rotate: {x: 5, y:40, z:0},
+    duration: 1500,
+    }); 
+
+sr.reveal('.apresentacao' , {
+    rotate: {x: 0, y:40, z:0},
+    duration: 1500} )
